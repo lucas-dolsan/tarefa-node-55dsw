dc8cc7004c4e8e23d0ec846bf3665de27af72762
Rodrigo <tazzsoni@gmail.com>
2020-04-20 22:18:24 -0300

    modificados cronograma.json e schemas referentes

--- src/schemas/atividadeSchema.js
+++ src/schemas/atividadeSchema.js
@@ -1,14 +1,15 @@
 const colaboradorSchema = require('./colaboradorSchema')
 const atividadeSchema = {
-    descricao: String,
-    observacao: String,
-    dataInicioAgendada: Date,
-    dataInicio: Date,
-    dataFimAgendada: Date,
-    dataFim: Date,
-    OS: Number,
-    AES: Number,
-    executor: colaboradorSchema
+  numeroItem: Number,
+  descricao: String,
+  observacao: String,
+  dataInicioAgendada: Date,
+  dataInicio: Date,
+  dataFimAgendada: Date,
+  dataFim: Date,
+  OS: Number,
+  AES: Number,
+  executor: colaboradorSchema
 }
 
 module.exports = atividadeSchema
--- src/schemas/cronagramaSchema.js
+++ src/schemas/cronagramaSchema.js
@@ -3,12 +3,13 @@ const colaboradorSchema = require("./colaboradorSchema")
 const atividadeSchema = require("./atividadeSchema")
 
 const cronogramaSchema = {
-    dataInicioAgendada: Date,
-    dataInicio: Date,
-    dataFimAgendada: Date,
-    dataFim: Date,
-    atividades: [atividadeSchema],
-    encarregado: colaboradorSchema,
+  nome: String,
+  dataInicioAgendada: Date,
+  dataInicio: Date,
+  dataFimAgendada: Date,
+  dataFim: Date,
+  atividades: [atividadeSchema],
+  encarregado: colaboradorSchema,
 }
 
 module.exports = cronogramaSchema
