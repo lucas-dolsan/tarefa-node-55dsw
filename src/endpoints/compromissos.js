73c29ff74fe169b8d038bb749309e4d7d8ec67aa
Lucas Dolsan <lucas.p@almode.com>
2021-07-09 21:32:55 -0300

    disabled authentication requirements

--- src/endpoints/compromissos.js
+++ src/endpoints/compromissos.js
@@ -2,7 +2,7 @@ const Endpoint = require("../classes/endpoint")
 const compromissoController = require("../controllers/compromissoController.js")
 
 module.exports = [
-    new Endpoint("/api/compromissos", "POST", compromissoController.create, { requiresAccessToken: true }),
-    new Endpoint("/api/compromissos", "GET", compromissoController.findAll, { requiresAccessToken: true }),
-    new Endpoint("/api/compromissos/:id", "GET", compromissoController.findById, { requiresAccessToken: true }),
+    new Endpoint("/api/compromissos", "POST", compromissoController.create, { requiresAccessToken: false }),
+    new Endpoint("/api/compromissos", "GET", compromissoController.findAll, { requiresAccessToken: false }),
+    new Endpoint("/api/compromissos/:id", "GET", compromissoController.findById, { requiresAccessToken: false }),
 ]
--- src/endpoints/usuarios.js
+++ src/endpoints/usuarios.js
@@ -2,5 +2,5 @@ const Endpoint = require("../classes/endpoint")
 const usuarioController = require('../controllers/usuarioController')
 
 module.exports = [
-    new Endpoint("/api/usuarios", "GET", usuarioController.findAll, { requiresAccessToken: true }),
+    new Endpoint("/api/usuarios", "GET", usuarioController.findAll, { requiresAccessToken: false }),
 ]
